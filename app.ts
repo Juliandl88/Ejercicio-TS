edfc97adafdab365ad64c185186bef9dc6e49832
Julián De Lisio <76756541+Juliandl88@users.noreply.github.com>
2022-01-12 21:42:29 -0300

    actualizacion

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
+
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
 let message: string = "Hello World";
 console.log(message);
